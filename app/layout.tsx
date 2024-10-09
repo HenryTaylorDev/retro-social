d20bc560ffa0920b711a42df83b4ece1e624b11c
HenryTaylorDev <hello@taylordev.co>
2024-10-09 15:56:12 +0100

    feat/added authjs and github oauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavMenu from "./NavMenu";
+import AuthProvider from "./AuthProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,11 +14,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <NavMenu />
-        {children}
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <div className="container">
+            <NavMenu />
+            {children}
+          </div>
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
